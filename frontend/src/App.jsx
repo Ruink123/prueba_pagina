bcd3c7f001f2e143cc295579f0840fff410caeb4
giancarlolucasramirez <josephandrew1906@gmail.com>
2025-05-15 15:51:44 -0600

    feat: :white_check_mark: se  creo el componente par ael foother y se reutilizó en app.jsx
    se  creo el componente par ael foother y se reutilizó en app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
 import Header from './Components/Header'
+import Footer from './Components/Foother'
 function App() {
 
   return (
     <>
       <Header />
+      <Footer />
+      
     </>
   )
 }
