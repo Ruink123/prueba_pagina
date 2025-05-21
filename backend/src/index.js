d3cab544971eebc78ffd7db9e0f007c1d4184426
Rudy Santizo <rudyextremox@gmail.com>
2025-05-21 10:33:16 -0600

    modificacion de index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor en puerto ${PORT}`);
-});
+const express = require('express');
+const app = express();
 
-console.log('Servidor funcionando');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en puerto ${PORT}`);
+});
