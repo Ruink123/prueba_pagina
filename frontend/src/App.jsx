81b0bc42493ff2486c88af19923357314ebcda40
giancarlolucasramirez <josephandrew1906@gmail.com>
2025-05-28 10:24:29 -0600

    feat: :zap: Se arregló el header para tener manejo de rutas y se agregaron los componentes Contacto,DonaHoy,Enlaces,Nosotros,rifa,Vidacfp
    cambios de header y se agregaron componentes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,59 @@ import Footer from "./Components/Footer";
 import Main from "./Components/Main";
 import Nosotros from "./Components/Nosotros";
 import { useState } from "react";
-
+import VidaCFP from "./Components/VidaCFP";
+import Enlaces from "./Components/Enlaces";
+import Rifa from "./Components/Rifa";
+import DonaHoy from "./Components/DonaHoy";
+import Contacto from "./Components/Contacto";
 function App() {
   const [vista, setVista] = useState("inicio"); // "inicio" o "nosotros"
 
   return (
     <>
-      <Header setVista={setVista} />
+      <Header setVista={setVista} vistaActiva={vista} />
+
       {vista === "inicio" ? (
         <>
           <Main />
           <Footer />
         </>
       ) : vista === "nosotros" ? (
-        <Nosotros />
-      ) : null}
+        <>
+          <Nosotros />
+          <Footer />
+        </>
+      ) : vista === "vidacfp" ? (
+        <>
+          <VidaCFP />
+          <Footer />
+        </>
+      )
+      : vista === "enlaces" ? (
+        <>
+          <Enlaces />
+          <Footer />
+        </>
+      )
+        : vista === "rifa" ? (
+        <>
+          <Rifa />
+          <Footer />
+        </>
+      )
+      : vista === "contacto" ? (
+        <>
+          <Contacto />
+          <Footer />
+        </>
+      )
+      : vista === "DonaHoy" ? (
+        <>
+          <DonaHoy />
+          <Footer  />
+        </>
+      )
+       : null}
     </>
   );
 }
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import Nosotros from "./Nosotros";
 
-const Header = ({setVista}) => {
+const Header = ({ setVista, vistaActiva }) => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <header className="bg-white md:py-4">
       <div className="w-full md:max-w-7xl mx-auto px-2 md:px-8 flex justify-center">
-        <nav className="flex items-center w-full md:relative justify-between h-20 md:h-auto gap-2 xs:gap-4">
+        <nav className="relative flex items-center w-full justify-between h-20 md:h-auto gap-2 xs:gap-4">
+          {" "}
           {/* Logo con margen izquierdo */}
           <div className="flex items-center flex-shrink-0 ml-2 xs:ml-4 sm:ml-6">
             <img
@@ -20,7 +20,7 @@ const Header = ({setVista}) => {
           </div>
           {/* Botón hamburguesa en la esquina superior derecha */}
           <button
-            className="md:hidden  right-2 top-6 xs:right-4 sm:right-6 z-50 transition-transform duration-500"
+            className="md:hidden absolute right-2 top-4 xs:right-4 sm:right-6 z-50 transition-transform duration-500"
             onClick={() => setMenuOpen(!menuOpen)}
             aria-label={menuOpen ? "Cerrar menú" : "Abrir menú"}
           >
@@ -62,82 +62,123 @@ const Header = ({setVista}) => {
             }
           >
             <li>
-              <button onClick={() => setVista("inicio")}
-                href="#"
-                className="flex items-center justify-center flex-1 h-10 px-4 bg-red-600 text-white rounded-lg text-lg  transition-all duration-200 hover:bg-white hover:text-red-600"
+              <button
+                onClick={() => setVista("inicio")}
+                className={`flex items-center justify-center flex-1 h-10 px-4 rounded-lg text-lg transition-all duration-200 ${
+                  vistaActiva === "inicio"
+                    ? "bg-red-600 text-white hover:bg-white hover:text-red-600"
+                    : "text-white hover:bg-white hover:text-[#3f59ac]"
+                }`}
               >
-                inicio
+                Inicio
               </button>
             </li>
             <li className="md:border-l border-white md:ml-4 md:pl-4">
               <button
                 onClick={() => setVista("nosotros")}
-                className="flex items-center justify-center flex-1 h-10 px-4 text-white rounded-lg text-lg transition-all duration-200 hover:bg-white hover:text-[#3f59ac]"
+                className={`flex items-center justify-center flex-1 h-10 px-4 rounded-lg text-lg transition-all duration-200 ${
+                  vistaActiva === "nosotros"
+                    ? "bg-red-600 text-white hover:bg-white hover:text-red-600"
+                    : "text-white hover:bg-white hover:text-[#3f59ac]"
+                }`}
               >
                 Nosotros
               </button>
             </li>
-            <li className="md:border-l border-white md:ml-4 md:pl-4 vida-cfp-width">
-              <a
-                href="#"
-                className="flex items-center justify-center flex-1 h-10 px-4 text-white rounded-lg text-lg transition-all duration-200 hover:bg-white hover:text-[#3f59ac]"
+            <li className="md:border-l border-white md:ml-4 md:pl-4 vida-cfp-width sm:w-36">
+              <button
+                onClick={() => setVista("vidacfp")}
+                className={`flex items-center justify-center flex-1 h-10 px-4 rounded-lg text-lg transition-all duration-200 ${
+                  vistaActiva === "vidacfp"
+                    ? "bg-red-600 text-white hover:bg-white hover:text-red-600"
+                    : "text-white hover:bg-white hover:text-[#3f59ac]"
+                }`}
               >
                 Vida en CFP
-              </a>
+              </button>
             </li>
-            <li className="md:border-l border-white md:ml-4 md:pl-4">
-              <a
-                href="#"
-                className="flex items-center justify-center flex-1 h-10 px-4 text-white rounded-lg text-lg transition-all duration-200 hover:bg-white hover:text-[#3f59ac]"
+            <li className="md:border-l border-white md:ml-4 md:pl-4 vida-cfp-width">
+              <button
+                onClick={() => setVista("enlaces")}
+                className={`flex items-center justify-center flex-1 h-10 px-4 rounded-lg text-lg transition-all duration-200 ${
+                  vistaActiva === "enlaces"
+                    ? "bg-red-600 text-white hover:bg-white hover:text-red-600"
+                    : "text-white hover:bg-white hover:text-[#3f59ac]"
+                }`}
               >
                 Enlaces
-              </a>
+              </button>
             </li>
-            <li className="md:border-l border-white md:ml-4 md:pl-4">
-              <a
-                href="#"
-                className="flex items-center justify-center flex-1 h-10 px-4 text-white rounded-lg text-lg  transition-all duration-200 hover:bg-white hover:text-[#3f59ac]"
+            <li className="md:border-l border-white md:ml-4 md:pl-4 vida-cfp-width">
+              <button
+                onClick={() => setVista("rifa")}
+                className={`flex items-center justify-center flex-1 h-10 px-4 rounded-lg text-lg transition-all duration-200 ${
+                  vistaActiva === "rifa"
+                    ? "bg-red-600 text-white hover:bg-white hover:text-red-600"
+                    : "text-white hover:bg-white hover:text-[#3f59ac]"
+                }`}
+              >
+                Rifa
+              </button>
+            </li>
+            <li className="md:border-l border-white md:ml-4 md:pl-4 vida-cfp-width">
+              <button
+                onClick={() => setVista("contacto")}
+                className={`flex items-center justify-center flex-1 h-10 px-4 rounded-lg text-lg transition-all duration-200 ${
+                  vistaActiva === "contacto"
+                    ? "bg-red-600 text-white hover:bg-white hover:text-red-600"
+                    : "text-white "
+                }`}
               >
                 Contacto
-              </a>
+              </button>
             </li>
-            <li className="relative group md:ml-4 md:pl-4">
-              {/* Fondo naranja */}
-              <span
-                className="absolute left-1 top-1 flex-1l h-full rounded-sm transition-all duration-200 group-hover:left-0 group-hover:top-0"
-                style={{
-                  background: "#ff3c00",
-                  zIndex: 0,
-                  transform: "rotate(-2deg)",
-                }}
-              ></span>
-              {/* Fondo amarillo */}
-              <span
-                className="absolute left-0 top-0 w-full h-full rounded-sm transition-all duration-200 group-hover:left-1 group-hover:top-1"
-                style={{
-                  background: "#ffd25a",
-                  zIndex: 1,
-                  transform: "rotate(2deg)",
-                }}
-              ></span>
-              <a
-                href="#"
-                className="relative flex items-center justify-center w-auto md:w-32 h-10 font-semibold px-4 py-1 text-base md:group-hover:bg-red-600"
-                style={{
-                  color: "#fff",
-                  zIndex: 2,
-                  fontWeight: 500,
-                }}
+            <li className="md:border-l border-white md:ml-4 md:pl-4 vida-cfp-width relative">
+              <button
+                onClick={() => setVista("DonaHoy")}
+                className={`relative group w-full ${
+                  vistaActiva === "DonaHoy"
+                    ? "bg-red-600 text-white hover:text-red-600"
+                    : "text-white hover:text-[#3f59ac]"
+                }`}
+                style={{ minWidth: "120px" }}
               >
-                Dona Hoy
+                {/* Fondo naranja */}
                 <span
-                  className="ml-2 text-xl transition-colors duration-200 group-hover:text-white"
-                  role="img"
-                  aria-label="corazón"
+                  className="absolute left-1 top-1 w-full h-full rounded-sm transition-all duration-200 group-hover:left-0 group-hover:top-0"
+                  style={{
+                    background: "#ff3c00",
+                    zIndex: 0,
+                    transform: "rotate(-2deg)",
+                  }}
+                ></span>
+                {/* Fondo amarillo */}
+                <span
+                  className="absolute left-0 top-0 w-full h-full rounded-sm transition-all duration-200 group-hover:left-1 group-hover:top-1"
+                  style={{
+                    background: "#ffd25a",
+                    zIndex: 1,
+                    transform: "rotate(2deg)",
+                  }}
+                ></span>
+                <span
+                  className="relative flex items-center justify-center w-auto md:w-32 h-10 font-semibold px-4 py-1 text-base md:group-hover:bg-red-600"
+                  style={{
+                    color: "#fff",
+                    zIndex: 2,
+                    fontWeight: 500,
+                  }}
                 >
-                  ♥
+                  Dona Hoy
+                  <span
+                    className="ml-2 text-xl transition-colors duration-200 group-hover:text-white"
+                    role="img"
+                    aria-label="corazón"
+                  >
+                    ♥
+                  </span>
                 </span>
-              </a>
+              </button>
             </li>
           </ul>
         </nav>
