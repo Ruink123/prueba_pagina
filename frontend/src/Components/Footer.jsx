3fd7bd2ac9ec95e9f2c7780015cb81af891328d7
giancarlolucasramirez <josephandrew1906@gmail.com>
2025-05-28 09:35:03 -0600

    style: :art: Se arregló el responsivo de footer
    Se arregló el responsivo de footer

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <>
-      <footer className="py-10 bg-[url(https://fundacionalbertomarvelli.org/wp-content/uploads/2022/05/bg2-2.jpg)] text-white footer-stack">
-        <div className="flex flex-col items-center gap-8 sm:gap-10 md:flex-row md:items-start md:justify-center md:gap-8 w-full max-w-5xl mx-auto px-4 md:max-w-4xl md:px-8 lg:max-w-5xl lg:px-4 ">
+      <footer className="  pt-10 sm:pt-1 bg-[url(https://fundacionalbertomarvelli.org/wp-content/uploads/2022/05/bg2-2.jpg)] text-white footer-stack flex flex-col justify-between">
+        <div className= "pb-10 py-4 w-full max-w-5xl mx-auto px-4 flex flex-col items-center gap-8 sm:gap-10 md:flex-row md:items-start md:justify-center md:gap-8 ">
           <div className="flex flex-col items-center text-center w-full md:w-1/3 md:items-start md:text-left">
             <strong>
               <h2 style={{ margin: 0 }}>Centro de Formación Profesional</h2>
@@ -27,17 +27,17 @@ const Footer = () => {
             <strong>
               <h2 style={{ margin: 0 }}>Contáctanos</h2>
             </strong>
-            <ul style={{ listStyle: "none", padding: 0, marginTop: "16px" }}>
-              <li>📍 39 Calle “B” 6-69 zona 8</li>
-              <li>📞 +502 2209-9999</li>
-              <li>✉️ info@fundacionalbertomarvelli.org</li>
-              <li>🔗 Facebook.com/CFP.AMBROSIO</li>
-              <li>🔗 Instagram.com/cfpbartolomeambrosio/</li>
+            <ul style={{ listStyle: "none", padding: 0, marginTop: "16px" }} className="flex flex-col gap-2" >
+              <li>📍  39 Calle “B” 6-69 zona 8</li>
+              <li>📞  +502 2209-9999</li>
+              <li>✉️  info@fundacionalbertomarvelli.org</li>
+              <li>🔗  Facebook.com/CFP.AMBROSIO</li>
+              <li>🔗  Instagram.com/cfpbartolomeambrosio/</li>
             </ul>
           </div>
         </div>
+      <div className=" w-full h-10 bg-black"></div>
       </footer>
-      <div className="w-full h-8 bg-black"></div>
     </>
   );
 };
